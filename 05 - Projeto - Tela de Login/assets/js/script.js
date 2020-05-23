d0df7b7bc528faf9a6fb994b9c39ef45bb3c567a
Kleber Nascimento <klebernascimento@outlook.com.br>
2020-05-23 13:16:25 -0300

    Projeto Tela de Login: trabalhando com javascript

---
+++ 05 - Projeto - Tela de Login/assets/js/script.js
@@ -0,0 +1,15 @@
+let nome = 'Kleber Alves de Amorim Nascimento'
+let email = 'klebernascimento@outlook.com.br'
+let github = 'www.github.com/kleber86'
+
+console.log(`
+  Criado por: ${nome}
+  Email: ${email}
+  Github: ${github}
+  `)
+
+  alert(`
+  Criado por: ${nome}
+  Email: ${email}
+  Github: ${github}
+  `)
