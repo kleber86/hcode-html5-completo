a1be98fc574033e67415ff77cca8a94b97258fcd
Kleber nascimento <klebernascimento@outlook.com.br>
2020-05-25 10:48:33 -0300

    Projeto Tabuada: usando funções e finalizando a tabuada

--- 06 - Projeto - Tabuada/tabuada.js
+++ 06 - Projeto - Tabuada/tabuada.js
@@ -1,22 +1,30 @@
-let tabuada = document.querySelector('#tabuada tbody')
-let valorA = 2
+function calculaTabuada(){
 
-for(let valorB = 0; valorB <= 10; valorB++){
+    let tabuada = document.querySelector('#tabuada tbody')
+    let valorA = parseInt(document.querySelector('#valorA').value)
+    
+    tabuada.innerHTML = ''
 
-    let resultado = valorA * valorB
-
-    let template = `
-        <td>${valorA}</td>
-        <td>x</td>
-        <td>${valorB}</td>
-        <td>=</td>
-        <td>${resultado}</td>
-`
-
-    let criarTR = document.createElement('tr')
-
-    criarTR.innerHTML = template
-
-    tabuada.append(criarTR)
-
+    for(let valorB = 0; valorB <= 10; valorB++){
+    
+        let resultado = valorA * valorB
+        
+        template = `
+            <td>${valorA}</td>
+            <td>x</td>
+            <td>${valorB}</td>
+            <td>=</td>
+            <td>${resultado}</td>
+    `
+    
+        let criarTR = document.createElement('tr')
+    
+        criarTR.innerHTML = template
+    
+        tabuada.append(criarTR)
+    }
 }
+
+calculaTabuada()
+
+document.querySelector('#valorA').addEventListener('change', calculaTabuada)
