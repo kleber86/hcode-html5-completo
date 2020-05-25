a06ae1031a59461b4815f14fc701dd579778a80e
Kleber nascimento <klebernascimento@outlook.com.br>
2020-05-25 11:09:06 -0300

    Projeto To-Do: trabalhando com arrays forEach

---
+++ 07 - Projeto - To Do/todo.js
@@ -0,0 +1,27 @@
+let data = [
+{
+    id: 1,
+    title: 'Estudar HTML'
+}, {
+    id: 2,
+    title: 'Estudar CSS'
+},
+{
+    id: 3,
+    title: 'Estudar Javascript'
+},
+{
+    id: 4,
+    title: 'Estudar React'
+}
+]
+
+data.forEach(task => {
+    let li = document.createElement('li')
+    li.innerHTML = `
+    <input type="checkbox" id="task-${task.id}">
+    <label for="task-${task.id}"> ${task.title}</label>
+    `
+
+    document.querySelector('.todo').append(li)
+})
