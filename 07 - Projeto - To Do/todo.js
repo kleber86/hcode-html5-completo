8169ab5901153ac97a242a237c195a005b9778f4
Kleber nascimento <klebernascimento@outlook.com.br>
2020-05-25 11:32:59 -0300

    Projeto To-Do: manipulando eventos do teclado e usando array

--- 07 - Projeto - To Do/todo.js
+++ 07 - Projeto - To Do/todo.js
@@ -1,35 +1,38 @@
-let data = [
-{
-    id: 1,
-    title: 'Estudar HTML'
-}, {
-    id: 2,
-    title: 'Estudar CSS'
-},
-{
-    id: 3,
-    title: 'Estudar Javascript'
-},
-{
-    id: 4,
-    title: 'Estudar React'
-}
-]
+let data = []
 
-data.forEach(task => {
-    let li = document.createElement('li')
-    li.innerHTML = `
-    <input type="checkbox" id="task-${task.id}">
-    <label for="task-${task.id}"> ${task.title}</label>
-    `
+function renderTodo(){
+    document.querySelector('.todo').innerHTML = ''
 
-    li.querySelector('input').addEventListener('change', e => {
-        if (e.target.checked){
-            li.classList.add('complete')
-        }else{
-            li.classList.remove('complete')
-        }
+    data.forEach(task => {
+        let li = document.createElement('li')
+        li.innerHTML = `
+        <input type="checkbox" id="task-${task.id}">
+        <label for="task-${task.id}"> ${task.title}</label>
+        `
+    
+        li.querySelector('input').addEventListener('change', e => {
+            if (e.target.checked){
+                li.classList.add('complete')
+            }else{
+                li.classList.remove('complete')
+            }
+        })
+    
+        document.querySelector('.todo').append(li)
     })
+}
 
-    document.querySelector('.todo').append(li)
+document.querySelector('#new-task').addEventListener('keyup', e =>{
+    if(e.key === 'Enter'){
+
+        data.push({
+            id: data.length + 1,
+            title: e.target.value
+        })
+
+        e.target.value = ''
+        renderTodo()
+    }
 })
+
+renderTodo()
