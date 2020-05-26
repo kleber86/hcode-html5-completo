3de1bc0e232009078416182f3cf81a663c87ca34
Kleber Alves <klebernascimento@outlook.com.br>
2020-05-26 12:54:16 -0300

    Projeto To-Do: usando Filter e finalizando o todo

--- 07 - Projeto - To Do/todo.js
+++ 07 - Projeto - To Do/todo.js
@@ -20,7 +20,19 @@ function renderTodo(){
         })
         
         li.querySelector('button').addEventListener('click', e=> {
-            console.dir(e.target.parentNode.querySelector('input ').id.split('-')[1])
+            let button = e.target
+            let li = button.parentNode
+            let input = li.querySelector('input')
+            let id = input.id
+            let idArray = id.split('-')
+            let todoId = idArray[1]
+            let title = li.querySelector('label').innerText
+
+            if(confirm(`Deseja realmente excluir a tarefa: ${title}?`)){
+                data = data.filter(task => task.id !== parseInt(todoId))
+
+                renderTodo()
+            }
         })
 
         document.querySelector('.todo').append(li)
