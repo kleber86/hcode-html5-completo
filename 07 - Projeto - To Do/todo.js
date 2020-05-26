13eb88225029d444cd0ebf2facfcc1c0b99bb965
Kleber Alves <klebernascimento@outlook.com.br>
2020-05-26 15:20:22 -0300

    Projeto To-Do: adicionando o botão de excluir

--- 07 - Projeto - To Do/todo.js
+++ 07 - Projeto - To Do/todo.js
@@ -6,8 +6,9 @@ function renderTodo(){
     data.forEach(task => {
         let li = document.createElement('li')
         li.innerHTML = `
-        <input type="checkbox" id="task-${task.id}">
-        <label for="task-${task.id}"> ${task.title}</label>
+            <input type="checkbox" id="task-${task.id}">
+            <label for="task-${task.id}"> ${task.title}</label>
+            <button type="button">x</button>    
         `
     
         li.querySelector('input').addEventListener('change', e => {
@@ -17,7 +18,11 @@ function renderTodo(){
                 li.classList.remove('complete')
             }
         })
-    
+        
+        li.querySelector('button').addEventListener('click', e=> {
+            console.warn('deletar?')
+        })
+
         document.querySelector('.todo').append(li)
     })
 }
